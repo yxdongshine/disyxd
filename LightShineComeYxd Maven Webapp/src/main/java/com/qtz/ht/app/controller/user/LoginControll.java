d00b71bd96a31f2f8abf249873e4e9924943073c
yxdongshine <659777399@qq.com>
2016-08-03 11:04:58 +0800

    yxdongshine <659777399@qq.com>

--- LightShineComeYxd Maven Webapp/src/main/java/com/qtz/ht/app/controller/user/LoginControll.java
+++ LightShineComeYxd Maven Webapp/src/main/java/com/qtz/ht/app/controller/user/LoginControll.java
@@ -1,12 +1,11 @@
 package com.qtz.ht.app.controller.user;
-
 import java.io.IOException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.dubbo.common.json.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.mall.core.common.response.RespJsonPHandler;
 import com.mall.core.exception.DaoException;
@@ -38,9 +37,13 @@ public class LoginControll {
 	public void login(@RequestParam String account,@RequestParam String pwd,HttpServletResponse resp,HttpServletRequest req) throws IOException {
 		try {
 			boolean isLogin = htUserService.login(account, pwd);//登录后生成新session
+			int loginValue= -1;
+			if(isLogin){
+				loginValue=0;
+			}
 			JSONObject result = new JSONObject();
-			result.put("isLogin", isLogin);
-		System.out.println(req.getLocalAddr());
+			result.put("loginValue", loginValue);
+		    System.out.println(req.getLocalAddr());
 			RespJsonPHandler.respOK(result, resp,req);
 		}catch (ServiceException e) {
 			log.error("网络错误", e);
@@ -51,23 +54,32 @@ public class LoginControll {
 		}
 	}
 	
-	@RequestMapping(value="register")
-	public void register(@RequestParam String account,@RequestParam String pwd,HttpServletResponse resp,HttpServletRequest req) throws IOException {
+	
+	/**
+	 * 
+	 * @param account
+	 * @param pwd
+	 * @param resp
+	 * @param req
+	 * @throws IOException
+	 */
+	@RequestMapping(value="add")
+	public void add(@RequestParam String account,@RequestParam String pwd,HttpServletResponse resp,HttpServletRequest req) throws IOException {
 		try {
 			HtUser htuser =null;
 			try {
 				htuser = htUserService.addUserVo(account, pwd);
 			} catch (DaoException e) {
 				// TODO Auto-generated catch block
-				RespJsonPHandler.respError(-1, "数据库异常", resp,req);
+				RespJsonPHandler.respError(-1, "已经注册", resp,req);
 			}//登录后生成新session
 			JSONObject result = new JSONObject();
 			result.put("htuser", htuser);
-		System.out.println(req.getLocalAddr());
+		    System.out.println(req.getLocalAddr()+result.getString("htuser").toString());
 			RespJsonPHandler.respOK(result, resp,req);
 		}catch (ServiceException e) {
 			log.error("网络错误", e);
-			RespJsonPHandler.respError(e.getErrorType(), e.getErrorTitle(), resp,req);
+			RespJsonPHandler.respError(e.getErrorType(), "已经注册", resp,req);
 		}catch(RpcException e){
 			log.error("网络错误", e);
 			RespJsonPHandler.respError(-1, "连接异常，请重试", resp,req);
